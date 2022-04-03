34bdd615d96f9fa8137142d891df802c557ac81f
kareemIt <kareem.itani2@gmail.com>
2022-04-02 23:32:12 -0400

    Working on instruction page

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const Instructions = () => {
-  return <div></div>;
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <p>Instructions</p>
+        <p>words</p>
+        <button className="close-button">+</button>
+      </div>
+    </div>
+  );
 };
 export default Instructions;
