08afa9e3299a5fedab35fbec98ce914329c4ed58
kareemIt <kareem.itani2@gmail.com>
2022-04-03 16:25:29 -0400

    fixed modal

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Instructions = () => {
+  const [modal, setModal] = useState(true);
+
+  const onClick = () => {
+    setModal(!modal);
+  };
+
   return (
-    <div className="modal">
+    <div className={modal ? 'modal' : 'modal-off'}>
       <div className="modal-content">
         <p>Instructions</p>
         <p>words</p>
-        <button className="close-button">+</button>
+        <button className="close-button" onClick={onClick}>
+          +
+        </button>
       </div>
     </div>
   );
