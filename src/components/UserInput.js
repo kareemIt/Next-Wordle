047309ba58901ff576aaaf2047d1e0e07e419987
kareemIt <kareem.itani2@gmail.com>
2022-04-04 16:26:41 -0400

    Axios request working

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,41 +1,50 @@
 import React, { useState, useRef, useEffect } from 'react';
+import Axios from 'axios';
 
 const UserInput = () => {
-  const [board, setBoard] = useState(['1', '2', '3', '4', '5', '6']);
-  let inputEl;
+  const [board, setBoard] = useState('');
+  const inputEl = useRef(null);
 
   const handleKeyDown = (e) => {
-    inputEl = { ...e.target.value };
-    const arr = inputEl.map((letter) => letter);
-    console.log(inputEl);
+    setBoard(e.currentTarget.value);
   };
 
   useEffect(() => {
-    const focus = (e) => inputEl && inputEl.current.focus();
+    const focus = (e) => {
+      if (inputEl.current) {
+        inputEl.current.focus();
+      }
+    };
     document.addEventListener('keydown', focus);
     return () => {
       document.removeEventListener('keydown', focus);
     };
   }, []);
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   if (inputEl.current.value.length < 5) {
-  //     return;
-  //   }
-  //   inputEl.current.value = '';
-  //   onEnter();
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputEl.current.value.length < 5) {
+      return;
+    }
+    const data = Axios.get(
+      'https://www.wordreference.com/es/translation.asp?tranword=' + board
+    );
+    data.then((value) => {
+      console.log(value.data.includes('fMatch = true'));
+    });
+    inputEl.current.value = '';
+  };
   return (
     <div>
       <div className="Game-board">
-        {board.map((letter, index) => (
+        {[...board.padEnd(5, ' ')].map((letter, index) => (
           <div className="user-letters">{letter}</div>
         ))}
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onInput={handleKeyDown}
+          ref={inputEl}
           type="text"
           maxLength="5"
           className="user-input"
