6f0573b85014b8476f60bcd6769799716d2cad27
kareemIt <kareem.itani2@gmail.com>
2022-04-07 14:24:36 -0400

    Css changes/Instruction rewording

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -13,23 +13,28 @@ const Instructions = ({ setStartScreen }) => {
       <div className="modal-content">
         <p>Instructions:</p>
         <p>Guess the word before you run out of time.</p>
-        <p>
-          There will be 8 randomly generatored letters that you have to use to
-          form a five letter-word.
-        </p>
-        <p>
-          There is a randomize posisiton button right below the 8 randomly
-          generated letters to help you move the letters around.
-        </p>
-        <p>
-          Each guess must be a valid five-letter word. Hit the enter button to
-          submit.
-        </p>
-        <p>
-          After each guess, if you put a valid five-letter word then you will
-          gain 10 seconds on the timer, if you put in a word that isn't real
-          then you will lose 5 seconds.
-        </p>
+        <ul>
+          <li>
+            There will be 8 randomly generatored letters that you have to use to
+            form a five letter-word.
+          </li>
+          <li>
+            There is a randomize position button will change the position of the
+            words
+          </li>
+          <li>
+            Each guess must be a valid five-letter word. Hit the enter button to
+            submit.
+          </li>
+          <li>
+            After each guess, if you put a valid five-letter word then you will
+            gain 10 seconds on the timer
+          </li>
+          <li>
+          If you put in a word that isn't real
+            then you will lose 5 seconds.
+            </li>
+        </ul>
         <p>A new Game will be available each day!</p>
         <button className="close-button" onClick={onClick}>
           Start
