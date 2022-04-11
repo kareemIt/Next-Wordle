c449dd7628148e31444e3822bdf034e40855f35a
kareemIt <kareem.itani2@gmail.com>
2022-04-11 09:38:22 -0400

    reworking random algo

--- src/utils/word.js
+++ src/utils/word.js
@@ -3,24 +3,11 @@ import { words, alphabet } from './WordBank.js';
 export const getRandomWord = () => {
   const randomWord = randomizePosition(words);
   const randomWords = [...randomWord].sort(() => Math.random() - 0.5);
-  const selectedShuffle = getRandomAlphabet(randomWords);
+  const shuffleAlphabet = alphabet.sort(() => 0.5 - Math.random());
+  const selectedShuffle = shuffleAlphabet.slice(0, 3);
   const combiningArrs = [...randomWords, ...selectedShuffle];
   const finalArr = combiningArrs.sort(() => Math.random() - 0.5);
   return finalArr;
-};
-
-const getRandomAlphabet = (array) => {
-  let finished = 0;
-  let selectedShuffled = [];
-  while (finished < 3) {
-    const shuffleAlphabet = alphabet.sort(() => 0.5 - Math.random());
-    const selectedShuffle = shuffleAlphabet[0];
-    if (!array.includes(selectedShuffle)) {
-      selectedShuffled.push(selectedShuffle);
-      finished++;
-    }
-  }
-  return selectedShuffled;
 };
 
 export const randomizePosition = (array) => {
